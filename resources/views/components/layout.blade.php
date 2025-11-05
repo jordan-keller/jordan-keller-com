ee0ace1abb24e9225a308997d0f51042cb86bdbe
Jordan Keller <me@jordankeller.work>
2025-11-05 09:12:58 -0500

    Initial commit: Laravel blog with YAML/Frontmatter and tagging (filterable)

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,7 +21,7 @@
         <!-- Navigation links -->
         <div id="navLinks" class="flex-col hidden flex lg:flex justify-between items-left">
             <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>     
-            <x-nav-link href="/blogs" :active="request()->is('blog')">Blog</x-nav-link>
+            <x-nav-link href="/blog" :active="request()->is('blog')">Blog</x-nav-link>
             <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
         </div>
     </nav>
